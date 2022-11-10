c0326e1351b347ef8e92639187114d578ba6fc91
Núria Ramoneda <nuriarai@gmail.com>
2022-11-10 21:40:23 +0100

    Add picture to create and rename it, gitignore assets/images

--- src/server/routers/itemsRouters/itemsRouters.ts
+++ src/server/routers/itemsRouters/itemsRouters.ts
@@ -1,13 +1,19 @@
 import express from "express";
+import multer from "multer";
 import {
   getItems,
   createItem,
 } from "../../controllers/itemsControllers/itemsControllers.js";
+import path from "path";
 
 // eslint-disable-next-line new-cap
 const itemsRouter = express.Router();
 
+const upload = multer({
+  dest: path.join("assets/images"),
+});
+
 itemsRouter.get("/list", getItems);
-itemsRouter.post("/create", createItem);
+itemsRouter.post("/create", upload.single("picture"), createItem);
 
 export default itemsRouter;
